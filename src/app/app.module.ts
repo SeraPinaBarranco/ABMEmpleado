cb669db2a7b1aa5150a8453182245768faa768a5
SeraPinaBarranco <arespin23@gmail.com>
2021-06-15 14:43:23 +0200

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddEditEmpleadoComponent } from './components/add-edit-empleado/add-edit-empleado.component';
+import { ListEmpleadoComponent } from './components/list-empleado/list-empleado.component';
+import { MensajeConfirmacionComponent } from './components/shared/mensaje-confirmacion/mensaje-confirmacion.component';
+
+//Modulo personalizado
+import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditEmpleadoComponent,
+    ListEmpleadoComponent,
+    MensajeConfirmacionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Slider angular material
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatSliderModule,
+    MatToolbarModule
+  ]
+})
+export class AngularMaterialModule { }
